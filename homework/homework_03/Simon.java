b2223fe7aec8aaa5e100f7708fdf14d464157a6b
Timothy Cherpes <54605266+tcherpes106@users.noreply.github.com>
2020-11-10 15:04:09 -0800

    added files for homework_03

---
+++ homework/homework_03/Simon.java
@@ -0,0 +1,116 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Simon.java
+ *  Purpose       :  Try to implement a classic memory game in java
+ *  @author       :  Timothy and Alex
+ *  Date written  :  11/10/2020
+ *  Description   :  This program tests a users memory skills by showing them characters and asking them to repeat it back
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+
+
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Arrays;
+import java.lang.Thread;
+
+public class Simon{
+
+  private int size;
+  private String blue = "B";
+  private String red = "R";
+  private String yellow = "Y";
+  private String green = "G";
+  private String[] simon = {blue, red, yellow, green};
+  private String[] gameArray = new String[100];
+  private int turn;
+
+  public Simon(){
+
+  }
+
+  public void arrayPrint(int turnplay){
+    for (int i = 0; i < turnplay; i++){
+      System.out.print(gameArray[i]);
+    }
+  }
+
+  public boolean arrayCompare(int turnPlay,String response){
+    String concat = "";
+    for (int i = 0; i< turnPlay; i++){
+      concat += gameArray[i];
+    }
+    if (concat.equals(response)){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  //simon game
+  public static void main(String[] args){
+    Simon game = new Simon();
+    ///random array generation
+    Random rand = new Random();
+    for (int i = 0; i< game.gameArray.length; i++){
+      int randInt = rand.nextInt(4);
+      game.gameArray[i] = game.simon[randInt];
+    }
+    //game begins
+    int turn = 1;
+    boolean play = true;
+    System.out.println("Welcome to Simon Says!\n");
+    System.out.println("Please type all responses with the matching case!\n");
+    while (play){
+      System.out.println("Here is the sequence.");
+      game.arrayPrint(turn);
+      try {
+        Thread.sleep(2000);
+      }
+      catch(InterruptedException ex) {
+        Thread.currentThread().interrupt();
+      }
+      for (int i=0;i < turn;i++){
+        System.out.print("\b");
+      }
+      for (int i=0;i < turn;i++){
+        System.out.print(" ");
+      }
+      System.out.print("\n");
+      System.out.println("What is the sequence?");
+      Scanner input = new Scanner(System.in);
+      String response = input.next();
+      try {
+        Thread.sleep(1000);
+      }
+      catch(InterruptedException ex) {
+        Thread.currentThread().interrupt();
+      }
+      for (int i=0;i < turn;i++){
+        System.out.print("\b");
+      }
+      for (int i=0;i < turn;i++){
+        System.out.print(" ");
+      }
+      System.out.print("\n");
+      if (game.arrayCompare(turn,response)){
+        System.out.println("Great job... Onto the next letter!");
+        turn++;
+      }
+      else {
+        System.out.println("Thanks for playing. Would you like to play again? (Yes/No)");
+        Scanner answer = new Scanner(System.in);
+        String finalResponse = answer.next();
+        if (finalResponse.equals("Yes")) {
+          play = true;
+          turn = 1;
+        }
+        else {
+          play = false;
+        }
+      }
+    }
+  }
+}
---
+++ homework/homework_03/oddEvenSort.java
@@ -0,0 +1,131 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  oddEvenSort.java
+ *  Purpose       :  To practice using sorting systems and sorting by using the odd-even sort method
+ *  @author       :  Timothy and Alex
+ *  Date written  :  11/10/2020
+ *  Description   :  To use this program, the user must type "java oddEvenSortApp"
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+class ArrayOddEven {
+   private long[] a;                        // ref to array a
+   private int nElems;                      // number of data items
+
+   public ArrayOddEven(int max) {
+      a = new long[max];
+      nElems = 0;
+   }
+
+   public void insert(long value) {
+      a[nElems] = value;
+      nElems++;
+   }
+
+   public void display() {
+      for(int j=0; j<nElems; j++)
+         System.out.print(a[j] + " ");
+      System.out.println("");
+   }
+
+   public void oddEvenSort() {
+     int odd, even;
+     for(int i = 0; i < nElems; i++){
+     //boolean sorted = false;
+     //while(!sorted){
+       //sorted = true;
+       for(odd = 1; odd < nElems; odd += 2){
+         if(nElems > 3)
+         if(a[odd] > a[odd + 1]){
+         swap(odd, odd + 1);
+         //sorted = false;
+       }
+     }
+     for(even = 0; even < nElems; even += 2){
+       if(a[even] > a[even + 1]){
+         swap(even, even + 1);
+         //sorted = false;
+       }
+     }
+    }
+   }
+
+   private void swap(int one, int two) {
+      long temp = a[one];
+      a[one] = a[two];
+      a[two] = temp;
+   }
+}
+
+class oddEvenSortApp {
+   public static void main(String[] args) {
+     System.out.println("\n-------Test 1-------\n");
+      int maxSize = 100;
+      ArrayOddEven arr;
+
+      arr = new ArrayOddEven(maxSize);
+      arr.insert(77);
+      arr.insert(99);
+      arr.insert(44);
+      arr.insert(55);
+      arr.insert(22);
+      arr.insert(88);
+      arr.insert(11);
+      arr.insert(00);
+      arr.insert(66);
+      arr.insert(33);
+
+      arr.display();
+
+      arr.oddEvenSort();
+      arr.display();
+
+      System.out.println("\n-------Test 2-------\n");
+      ArrayOddEven arr2 = new ArrayOddEven(maxSize);
+
+      arr2.insert(124);
+      arr2.insert(21);
+      arr2.insert(02);
+      arr2.insert(524);
+      arr2.insert(213);
+      arr2.insert(69);
+      arr2.insert(25);
+      arr2.insert(00);
+      arr2.insert(01);
+
+      arr2.display();
+
+      arr2.oddEvenSort();
+      arr2.display();
+
+      System.out.println("\n-------Test 3-------");
+      //test to make sure oddEvenSort works when less than 3 nElems
+      ArrayOddEven arr3 = new ArrayOddEven(maxSize);
+
+      arr3.insert(35);
+      arr3.insert(07);
+
+      arr3.display();
+
+      arr3.oddEvenSort();
+      arr3.display();
+
+      System.out.println("\n-------Test 4-------");
+      //already sorted
+      ArrayOddEven arr4 = new ArrayOddEven(maxSize);
+      arr4.insert(0);
+      arr4.insert(01);
+      arr4.insert(02);
+      arr4.insert(03);
+      arr4.insert(04);
+      arr4.insert(05);
+      arr4.insert(06);
+      arr4.insert(07);
+      arr4.insert(8);
+      arr4.insert(9);
+
+      arr4.display();
+
+      arr4.oddEvenSort();
+      arr4.display();
+   }
+}
